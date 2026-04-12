380f2eedf358a7cd2d71c12242c9ccb7577529ba
Abhinav Singh <official.abhinavsingh15@gmail.com>
2026-04-12 08:08:35 +0530

    added extenstions

---
+++ app/documents/[documentId]/editor.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { TableKit } from "@tiptap/extension-table";
+import Image from "@tiptap/extension-image";
+
+export const Editor = () => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class:
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-240 pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TableKit.configure({
+        table: { resizable: true },
+      }),
+      Image.configure({
+        resize: {
+          enabled: true,
+          // directions: ["top", "bottom", "left", "right"], // can be any direction or diagonal combination
+          // minWidth: 50,
+          // minHeight: 50,
+          alwaysPreserveAspectRatio: true,
+        },
+      }),
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
+    immediatelyRender: false,
+  });
+
+  return (
+    <div className="size-full overflow-x-auto flex justify-center bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-204 py-4 print:py-4">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
